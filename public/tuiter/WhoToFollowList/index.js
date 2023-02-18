20bf81ab197aa7660b9d3206e6672f08d5011518
cmallen8 <allen.christ@northeastern.edu>
2023-02-18 16:06:50 -0500

    a5 second commit

---
+++ public/tuiter/WhoToFollowList/index.js
@@ -0,0 +1,19 @@
+import WhoToFollowListItem from "./WhoToFollowListItem.js";
+import who from "./who.js";
+
+const WhoToFollowList = () =>{
+    return (`
+        <ul class="list-group">
+            <li class="list-group-item">
+                <span class="fw-bold">Who to follow</span>
+            </li>
+            ${
+                who.map(who => {
+                    return(WhoToFollowListItem(who));
+                }).join('')
+            }
+        </ul>
+    `);
+}
+
+export default WhoToFollowList;
